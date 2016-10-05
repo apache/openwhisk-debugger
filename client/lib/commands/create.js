58a07d4505ec0a58a8f9fad9a0c8cd52d065ef87
Nick M. Mitchell <nickm@us.ibm.com>
2016-10-05 12:12:08 -0400

    update create command to take kind and code on the command line

--- client/lib/commands/create.js
+++ client/lib/commands/create.js
@@ -26,30 +26,40 @@ exports.created = {};
  * Create an action
  *
  */
-exports.create = function create(wskprops, next, name) {
+exports.create = function create() {
+    var wskprops = arguments[0];
+    var next = arguments[1];
+    var name = arguments[2];
+    var kind = arguments[3];
+    var code = arguments.length > 4 && Array.from(arguments).slice(4).join(' ');
+
     var questions = [];
     if (!name) {
 	questions.push({ name: 'name', message: 'Choose a name for your new action' });
     }
-    questions.push({ name: 'kind', type: 'list',
-		     message: 'Which runtime do you want to use?',
-		     choices: ['nodejs', 'swift', 'python' ]
-		   });
-    questions.push({ name: 'code', type: 'editor',
-		     message: 'Please provide the function body for your new action',
-		     default: function(response) {
-			 if (response.kind === 'nodejs') {
-			     // nodejs
-			     return 'function main(params) {\n    return { message: \'hello\' };\n}\n';
-			 } else if (response.kind === 'swift') {
-			     // swift
-			     return 'func main(args: [String:Any]) -> [String:Any] {\n      return [ "message" : "Hello world" ]\n}\n';
-			 } else {
-			     // python
-			     return 'import sys\n\ndef main(dict):\n    return { \'message\': \'Hello world\' }\n';
+    if (!kind) {
+	questions.push({ name: 'kind', type: 'list',
+			 message: 'Which runtime do you want to use?',
+			 choices: ['nodejs', 'swift', 'python' ]
+		       });
+    }
+    if (!code) {
+	questions.push({ name: 'code', type: 'editor',
+			 message: 'Please provide the function body for your new action',
+			 default: function(response) {
+			     if (response.kind === 'nodejs') {
+				 // nodejs
+				 return 'function main(params) {\n    return { message: \'hello\' };\n}\n';
+			     } else if (response.kind === 'swift') {
+				 // swift
+				 return 'func main(args: [String:Any]) -> [String:Any] {\n      return [ "message" : "Hello world" ]\n}\n';
+			     } else {
+				 // python
+				 return 'import sys\n\ndef main(dict):\n    return { \'message\': \'Hello world\' }\n';
+			     }
 			 }
-		     }
-		   });
+		       });
+    }
 
     inquirer
 	.prompt(questions)
@@ -58,8 +68,8 @@ exports.create = function create(wskprops, next, name) {
 		  actionName: name || response.name,
 		  action: {
 		      exec: {
-			  kind: response.kind,
-			  code: response.code
+			  kind: kind || response.kind,
+			  code: code || response.code
 		      }
 		  }
 	      });
