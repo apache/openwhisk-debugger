04714d924e11cb03f5011023be5d80c700a94843
Nick M. Mitchell <nickm@us.ibm.com>
2016-10-11 21:03:01 -0400

    add coyright notice checker to the tests

---
+++ client/util/scanCode.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+scan='grep --exclude-dir=node_modules --exclude-dir=deps -rL --exclude *~ --exclude *.json --exclude *.run --exclude *.md Copyright ./*'
+
+if [ `${scan}  | wc -l` != 0 ]; then
+    echo "Failure. These files do no have a copyright notice:"
+    (${scan})
+    exit 1
+fi
