aa299a6554746bbcf3a5211fb8aedb625302e7bd
Nick M. Mitchell <nickm@us.ibm.com>
2016-10-12 21:02:30 -0400

    update scanCode to ignore pyc

--- client/util/scanCode.sh
+++ client/util/scanCode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-scan='grep --exclude-dir=node_modules --exclude-dir=deps -rL --exclude *~ --exclude *.json --exclude *.run --exclude *.md Copyright ./*'
+scan='grep --exclude-dir=node_modules --exclude-dir=deps -rL --exclude *~ --exclude *.json --exclude *.run --exclude *.md --exclude *.pyc Copyright ./*'
 
 if [ `${scan}  | wc -l` != 0 ]; then
     echo "Failure. These files do no have a copyright notice:"
