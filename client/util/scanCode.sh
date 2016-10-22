2b8a29d854b73b87df9af485136a057ef4f3630b
Nick M. Mitchell <nickm@us.ibm.com>
2016-10-22 09:00:36 -0400

    fix scanCode -- was glob expanding too early

--- client/util/scanCode.sh
+++ client/util/scanCode.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-scan='grep --exclude-dir=node_modules --exclude-dir=deps -rL --exclude *~ --exclude *.json --exclude *.run --exclude *.md --exclude *.pyc Copyright ./*'
+set -o noglob
+scan='grep --exclude-dir=node_modules --exclude-dir=deps -rL --exclude *~ --exclude *.json --exclude *.run --exclude *.md --exclude *.pyc --exclude .* Copyright .'
 
-if [ `${scan}  | wc -l` != 0 ]; then
+if [ `${scan} | wc -l` != 0 ]; then
     echo "Failure. These files do no have a copyright notice:"
     (${scan})
     exit 1
